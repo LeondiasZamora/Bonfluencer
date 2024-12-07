5180c4502218cbb5466dee08ce92ec5497387ba1
Leonidas Zamora <leonidas.zamora.gottwald@gmail.com>
2024-12-07 16:42:14 +0100

    you can add a comment now
    the user you add it to is still hardcoded >>>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,29 @@ class ReviewsController < ApplicationController
     @reviews = current_user.reviews
   end
 
+  def home
+  end
+
+  def new
+    @review = Review.new
+    @review.user = User.first
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.author = current_user
+    @review.user = User.first
+
+    if @review.save
+      redirect_to home_path, notice: "You made someone happy today :)"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
