2d4fd959a0930548a7f755df27fcf1e97c16189d
Leonidas Zamora <leonidas.zamora.gottwald@gmail.com>
2024-12-11 13:07:51 +0100

    basic setup for follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   has_many :reviews
+  has_many :followers, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy
+  # Users that this user is following
+  has_many :following, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
+
+  # Convenience methods for easier access
+  has_many :followers_users, through: :followers, source: :follower
+  has_many :following_users, through: :following, source: :followed
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
