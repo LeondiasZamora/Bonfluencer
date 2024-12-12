8f3c0362e447b5e7cff838328917ddb5b303ed0a
Leonidas Zamora <leonidas.zamora.gottwald@gmail.com>
2024-12-12 23:05:18 +0100

    now the follow page exists
    there is some weird functionality that whenever I try to link to the folllowers page it does not let me. I have to take alook at this bug

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,4 +1,9 @@
 class FollowsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @followers = User.where(followed: @user)
+  end
+
   def create
     @user = User.find(params[:user_id])
     current_user.following.create(followed: @user)
@@ -9,5 +14,9 @@ class FollowsController < ApplicationController
     @user = User.find(params[:user_id]) # User to unfollow
     current_user.following.find_by(followed: @user).destroy # Remove the follow relationship
     redirect_to @user
+  end
+
+  def followers
+    @user = User.find(params[:user_id])
   end
 end
