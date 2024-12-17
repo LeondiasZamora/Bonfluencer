c9f50a7279c0f2287266e2af06fc3193b7d8fd99
Leonidas Zamora <leonidas.zamora.gottwald@gmail.com>
2024-12-17 19:37:54 +0100

    I simply cannot overwrite the devise controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @reviews = @user.reviews.order(created_at: :desc)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to home_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :photo)
+  end
 end
